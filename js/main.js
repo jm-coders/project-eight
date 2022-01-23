478ac04172c7fcb687e6bb14c79e449230a88d5d
jm <mdjubaerm16247@gmail.com>
2022-01-23 11:06:16 +0600

    Complete Project With Responsive

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+function openNav() {
+        document.getElementById("myNav").style.width = "100%";
+}
+function closeNav() {
+        document.getElementById("myNav").style.width = "0%";
+}
 $(document).ready(function(){
     $(".stcky_menu").waypoint(function(direction){
         if (direction == "down"){
@@ -6,4 +12,28 @@ $(document).ready(function(){
             $(".top_header").removeClass("stcky");
         };
     });
+
+    
+// Active link
+
+$("nav ul li a").click(function(){
+    $("nav ul li a").removeClass("active");
+    $(this).addClass("active");
+});
+
+// SMOOTH Scroll FOR safari/IE/edge
+
+$("a").on('click',function(event){
+    if (this.hash !== "") {
+        event.preventDefault();
+
+        var hash = this.hash;
+
+        $('html,body').animate({
+            scrollTop: $(hash).offset().top
+        }, 500, function(){
+            window.location.hash = hash;
+        })
+    }
+});
 });
